196a9f4f9c37436f2ea2e5748a69872e64f0231b
taise <taise515@gmail.com>
2016-08-27 21:04:49 +0900

    Add: EventMachine::Pool example code

---
+++ ruby/em_pool_sample.rb
@@ -0,0 +1,35 @@
+require 'eventmachine'
+require 'em-http-request'
+
+EM.run do
+  pool  = EM::Pool.new
+  spawn = lambda { pool.add EM::HttpRequest.new('http://example.org') }
+  p EM::HttpRequest.new('http://example.org')
+  10.times { spawn[] }
+  done, scheduled = 0, 0
+
+  check = lambda do
+    done += 1
+    puts "done: #{done}, scheduled: #{scheduled}"
+    if done >= scheduled
+      EM.stop
+    end
+  end
+
+  pool.on_error { |conn| spawn[] }
+
+  100.times do |i|
+    pool.perform do |conn|
+      req = conn.get :path => '/', :keepalive => true
+
+      req.callback do
+        p [:success, conn.object_id, i, req.response.size]
+        check[]
+      end
+
+      req.errback { check[] }
+
+      req
+    end
+  end
+end
