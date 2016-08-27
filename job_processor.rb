acae4ed4a6b9912f7437410f4606b8a1bf740a86
taise <taise515@gmail.com>
2016-08-27 22:23:58 +0900

    Add: JobProcessor#last_time & #color

--- job_processor.rb
+++ job_processor.rb
@@ -6,11 +6,31 @@ class JobProcessor
   SUCCESS = 0
   ERROR = 1
 
+  attr_reader :color, :last_time
+  def initialize
+    @@count ||= 0
+    @last_time
+  end
+
+  def self.count=(n)
+    @@count = n
+  end
+
   def process(time)
+    @last_time = time
+    puts "oid: #{object_id}, time: #{time}"
     if time == 2
       return ERROR
     end
     sleep time
     return SUCCESS
+  end
+
+  def color
+    if @color.nil?
+      @color = [:red, :blue, :green][@@count % 3]
+      @@count += 1
+    end
+    @color
   end
 end
--- test/job_processor_test.rb
+++ test/job_processor_test.rb
@@ -2,20 +2,33 @@ require 'minitest/autorun'
 require './worker'
 
 class JobProcessorTest < Minitest::Test
+  def fixed_values
+    [JobProcessor::SUCCESS, JobProcessor::ERROR]
+  end
+
   def test_fixed_values
     assert_equal 0, JobProcessor::SUCCESS
     assert_equal 1, JobProcessor::ERROR
   end
 
-  def fixed_values
-    [JobProcessor::SUCCESS, JobProcessor::ERROR]
+  def test_initialized
+    assert_equal nil,   JobProcessor.new.last_time
+
+    JobProcessor.count = 0
+    assert_equal :red,   JobProcessor.new.color
+    assert_equal :blue,  JobProcessor.new.color
+    assert_equal :green, JobProcessor.new.color
   end
 
   def test_process
-    time, return_code = JobProcessor.new.process(1)
+    job_processor = JobProcessor.new
+    return_code = job_processor.process(1)
     assert_equal  JobProcessor::SUCCESS, return_code
+    assert_equal  1, job_processor.last_time
 
-    time, return_code = JobProcessor.new.process(2)
+    job_processor = JobProcessor.new
+    return_code = job_processor.process(2)
     assert_equal  JobProcessor::ERROR, return_code
+    assert_equal  2, job_processor.last_time
   end
 end
